d5c156647870f15dd2fb07ed3736c1619d23db29
forward <qq1506430045@163.com>
2023-11-02 14:08:14 +0800

    opbnb-mainnet

--- packages/indexer/src/config/network.ts
+++ packages/indexer/src/config/network.ts
@@ -692,6 +692,40 @@ export const getNetworkSettings = (): NetworkSettings => {
         },
       };
     }
+    // opbnb
+    case 204: {
+      return {
+        ...defaultNetworkSettings,
+        isTestnet: false,
+        enableWebSocket: true,
+        realtimeSyncMaxBlockLag: 32,
+        realtimeSyncFrequencySeconds: 5,
+        lastBlockLatency: 5,
+        headBlockDelay: 10,
+        onStartup: async () => {
+          // Insert the native currency
+          await Promise.all([
+            idb.none(
+                `
+                INSERT INTO currencies (
+                  contract,
+                  name,
+                  symbol,
+                  decimals,
+                  metadata
+                ) VALUES (
+                  '\\x0000000000000000000000000000000000000000',
+                  'Ether',
+                  'ETH',
+                  18,
+                  '{"coingeckoCurrencyId": "ethereum", "image": "https://assets.coingecko.com/coins/images/279/large/ethereum.png"}'
+                ) ON CONFLICT DO NOTHING
+              `
+            ),
+          ]);
+        },
+      };
+    }
     // ZKsync
     case 324: {
       return {
